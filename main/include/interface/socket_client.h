82e4739589637f0668844c05d78935efcce329b9
splee777 <splee@sandbox.org>
2024-11-12 21:18:54 -0500

    Add files via upload

---
+++ main/include/interface/socket_client.h
@@ -0,0 +1,9 @@
+#ifndef ESP32_XBEE_SOCKET_CLIENT_H
+#define ESP32_XBEE_SOCKET_CLIENT_H
+
+#include <stdint.h>
+#include <esp_event_base.h>
+
+void socket_client_init();
+
+#endif //ESP32_XBEE_SOCKET_CLIENT_H
